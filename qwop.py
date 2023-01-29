afde08e5dda81a1ec5395df2cf50ea7980af776a
orosmatthew <orosmatthew@pm.me>
2023-01-28 20:28:21 -0500

    Create character class

--- qwop.py
+++ qwop.py
@@ -52,31 +52,64 @@ class PhysicsLimbWithMuscle:
         self.muscle.stiffness = 0.0
 
 
+class Character:
+
+    def __init__(self, physics_space: pm.Space, leg_muscle_strength: float):
+        self.leg_muscle_strength = leg_muscle_strength
+
+        self.left_foot = PhysicsLimb(physics_space, group=1, width=30, height=20, mass=5, friction=0.6,
+                                     position=(150, 300))
+
+        self.left_leg = PhysicsLimbWithMuscle(physics_space, group=1, width=15, height=100, mass=10, friction=0.6,
+                                              position=(150, 300))
+
+        self.left_ankle = pm.PivotJoint(self.left_foot.body, self.left_leg.limb.body, (0, 25), (0, -45))
+        physics_space.add(self.left_ankle)
+        self.left_ankle_limit = pm.RotaryLimitJoint(self.left_foot.body, self.left_leg.limb.body, radians(-25),
+                                                    radians(25))
+        physics_space.add(self.left_ankle_limit)
+
+        self.right_foot = PhysicsLimb(physics_space, group=1, width=30, height=20, mass=5, friction=0.6,
+                                      position=(250, 300))
+
+        self.right_leg = PhysicsLimbWithMuscle(physics_space, group=1, width=15, height=100, mass=10, friction=0.8,
+                                               position=(250, 300))
+
+        self.right_ankle = pm.PivotJoint(self.right_foot.body, self.right_leg.limb.body, (0, 25), (0, -45))
+        physics_space.add(self.right_ankle)
+        self.right_ankle_limit = pm.RotaryLimitJoint(self.right_foot.body, self.right_leg.limb.body, radians(-25),
+                                                     radians(25))
+        physics_space.add(self.right_ankle_limit)
+
+        self.hip = pm.PivotJoint(self.left_leg.limb.body, self.right_leg.limb.body, (0, 50), (0, 50))
+        physics_space.add(self.hip)
+        self.hip_limit = pm.RotaryLimitJoint(self.left_leg.limb.body, self.right_leg.limb.body, radians(-120),
+                                             radians(120))
+        physics_space.add(self.hip_limit)
+
+    def draw(self) -> None:
+        self.left_foot.draw(rl.MAROON)
+        self.right_foot.draw(rl.RED)
+        self.left_leg.limb.draw(rl.GRAY)
+        self.right_leg.limb.draw(rl.WHITE)
+
+    def move_legs_q(self) -> None:
+        self.left_leg.move_muscle(self.leg_muscle_strength, -radians(50))
+        self.right_leg.move_muscle(self.leg_muscle_strength, radians(50))
+
+    def move_legs_w(self) -> None:
+        self.left_leg.move_muscle(self.leg_muscle_strength, radians(50))
+        self.right_leg.move_muscle(self.leg_muscle_strength, -radians(50))
+
+    def relax_legs(self) -> None:
+        self.left_leg.relax_muscle()
+        self.right_leg.relax_muscle()
+
+
 space = pm.Space()
 space.gravity = (0, -900.0)
 
-left_foot = PhysicsLimb(space, group=1, width=30, height=20, mass=5, friction=0.6, position=(150, 300))
-
-left_leg = PhysicsLimbWithMuscle(space, group=1, width=15, height=100, mass=10, friction=0.6, position=(150, 300))
-
-pivot_left_ankle = pm.PivotJoint(left_foot.body, left_leg.limb.body, (0, 25), (0, -45))
-space.add(pivot_left_ankle)
-rotary_limit_left_ankle = pm.RotaryLimitJoint(left_foot.body, left_leg.limb.body, radians(-25), radians(25))
-space.add(rotary_limit_left_ankle)
-
-right_foot = PhysicsLimb(space, group=1, width=30, height=20, mass=5, friction=0.6, position=(250, 300))
-
-right_leg = PhysicsLimbWithMuscle(space, group=1, width=15, height=100, mass=10, friction=0.8, position=(250, 300))
-
-pivot_right_ankle = pm.PivotJoint(right_foot.body, right_leg.limb.body, (0, 25), (0, -45))
-space.add(pivot_right_ankle)
-rotary_limit_right_ankle = pm.RotaryLimitJoint(right_foot.body, right_leg.limb.body, radians(-25), radians(25))
-space.add(rotary_limit_right_ankle)
-
-pivot_hip = pm.PivotJoint(left_leg.limb.body, right_leg.limb.body, (0, 50), (0, 50))
-space.add(pivot_hip)
-rotary_limit_hip = pm.RotaryLimitJoint(left_leg.limb.body, right_leg.limb.body, radians(-120), radians(120))
-space.add(rotary_limit_hip)
+character = Character(space, leg_muscle_strength=1500000.0)
 
 ground_body = pm.Body(body_type=pm.Body.STATIC)
 ground_body.position = 500, 0
@@ -99,31 +132,24 @@ rl.init_window(1280, 720, "QWOP-BOT")
 while not rl.window_should_close():
     space.step(1.0 / 60.0)
 
-    camera.target = rl.Vector2(left_leg.limb.body.position.x, -left_leg.limb.body.position.y)
+    camera.target = rl.Vector2(character.left_leg.limb.body.position.x, -character.left_leg.limb.body.position.y)
 
     rl.begin_drawing()
     rl.begin_mode_2d(camera)
 
     rl.clear_background(rl.BLACK)
 
-    left_foot.draw(rl.MAROON)
-    right_foot.draw(rl.RED)
-    left_leg.limb.draw(rl.GRAY)
-    right_leg.limb.draw(rl.WHITE)
+    character.draw()
 
     rl.draw_rectangle_pro(rl.Rectangle(round(ground_body.position.x), round(-ground_body.position.y), 1000, 50),
                           rl.Vector2(1000 / 2, 50 / 2), 0.0, rl.GREEN)
 
-    leg_muscle_strength = 1500000.0
     if rl.is_key_down(rl.KeyboardKey.KEY_Q):
-        left_leg.move_muscle(leg_muscle_strength, -radians(50))
-        right_leg.move_muscle(leg_muscle_strength, radians(50))
+        character.move_legs_q()
     elif rl.is_key_down(rl.KeyboardKey.KEY_W):
-        left_leg.move_muscle(leg_muscle_strength, radians(50))
-        right_leg.move_muscle(leg_muscle_strength, -radians(50))
+        character.move_legs_w()
     else:
-        left_leg.relax_muscle()
-        right_leg.relax_muscle()
+        character.relax_legs()
 
     rl.end_mode_2d()
     rl.end_drawing()
