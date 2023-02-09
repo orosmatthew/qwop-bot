57c4dd5817567b0aed3de2f5863b4d4fb0830aa0
Michael Terekhov <mterekho20@bw.edu>
2023-02-09 13:25:49 -0500

    Create rotary limits for neck, legs, arms

--- qwop.py
+++ qwop.py
@@ -76,60 +76,75 @@ class PhysicsLimbWithMuscle:
 class Character:
     def __init__(self, physics_space: pm.Space, leg_muscle_strength: float):
         
-        ###
-
-
-
-
-        self.torso = PhysicsLimb(physics_space, group=2, width=40, height=150, mass=0.5, friction=0.6, 
+        ### Body parts
+        self.torso = PhysicsLimb(physics_space, group=1, width=40, height=150, mass=0.5, friction=1, 
                                     position=(200,600))
 
-        self.neck = PhysicsLimbWithMuscle(physics_space, group=4, width=20, height=10, mass=0.2, friction=0.2, 
+        self.neck = PhysicsLimbWithMuscle(physics_space, group=1, width=20, height=10, mass=0.05, friction=1, 
                                     position=(230,630))
 
-        self.head = PhysicsLimb(physics_space, group=3, width=40, height=40, mass=0.3, friction=0.2, 
+        self.head = PhysicsLimb(physics_space, group=1, width=40, height=40, mass=0.3, friction=1, 
                                     position=(230,650))
+        
+        self.right_bicepts = PhysicsLimbWithMuscle(physics_space, group=1, width=15, height=80, mass=0.01, friction=0.6,
+                                                    position=(230,400))
+        self.left_bicepts = PhysicsLimbWithMuscle(physics_space, group=1, width=15, height=80, mass=0.01, friction=0.6,
+                                                    position=(230,400))
+
+        self.right_forearm = PhysicsLimb(physics_space, group=1, width=15, height=70, mass=0.01, friction=0.6,
+                                                    position=(230,400))
+        self.left_forearm = PhysicsLimb(physics_space, group=1, width=15, height=70, mass=0.1, friction=0.6,
+                                                    position=(230,400))
 
         
-
+        ### Connection(Joints)
         self.neck_to_torso =  pm.PivotJoint(self.torso.body, self.neck.limb.body, (0,75), (0,-5))  ##The anchor point on body a, the anchor point on body b; for the last two variables
-      
+        
         self.neck_to_head = pm.PivotJoint(self.head.body, self.neck.limb.body, (0,-20), (0,5))
         
-
-        physics_space.add(self.neck_to_torso)
-        physics_space.add(self.neck_to_head)
-
-        self.right_bicepts = PhysicsLimbWithMuscle(physics_space, group=2, width=15, height=80, mass=0.01, friction=0.6,
-                                                    position=(230,400))
-        self.left_bicepts = PhysicsLimbWithMuscle(physics_space, group=2, width=15, height=80, mass=0.01, friction=0.6,
-                                                    position=(230,400))
-
-        self.right_forearm = PhysicsLimb(physics_space, group=2, width=15, height=70, mass=0.01, friction=0.6,
-                                                    position=(230,400))
-        self.left_forearm = PhysicsLimb(physics_space, group=2, width=15, height=70, mass=0.1, friction=0.6,
-                                                    position=(230,400))
-
         self.right_bicepts_to_forearm = pm.PivotJoint(self.right_bicepts.limb.body, self.right_forearm.body, (0,-40), (0,35))
+        
         self.left_bicepts_to_forearm = pm.PivotJoint(self.left_bicepts.limb.body, self.left_forearm.body, (0,-40), (0,35))
+
+        self.right_arm_to_torso = pm.PivotJoint(self.right_bicepts.limb.body, self.torso.body, (3, 45), (0,66))
+
+        self.left_arm_to_torso = pm.PivotJoint(self.left_bicepts.limb.body, self.torso.body, (-3, 45), (0,66))
 
         physics_space.add(self.right_bicepts_to_forearm)
         physics_space.add(self.left_bicepts_to_forearm)
 
-        self.right_arm_to_torso = pm.PivotJoint(self.right_bicepts.limb.body, self.torso.body, (3, 45), (0,66))
-        self.left_arm_to_torso = pm.PivotJoint(self.left_bicepts.limb.body, self.torso.body, (-3, 45), (0,66))
-
+        physics_space.add(self.neck_to_torso)
+        physics_space.add(self.neck_to_head)
+        
         physics_space.add(self.right_arm_to_torso)
         physics_space.add(self.left_arm_to_torso)
 
-        self.right_elbow_limit = pm.RotaryLimitJoint(self.right_bicepts.limb.body, self.right_forearm.body, radians(30),
-                                              radians(155))
+        #Limits for the elbows, shoulders, neck (to body and to head)
+        self.right_elbow_limit = pm.RotaryLimitJoint(self.right_bicepts.limb.body, self.right_forearm.body, radians(20),
+                                            radians(155))
 
-        self.left_elbow_limit = pm.RotaryLimitJoint(self.left_bicepts.limb.body, self.left_forearm.body, radians(30),
-                                              radians(155))
+        self.left_elbow_limit = pm.RotaryLimitJoint(self.left_bicepts.limb.body, self.left_forearm.body, radians(20),
+                                            radians(155))
+        
+        self.right_shoulder_limit = pm.RotaryLimitJoint(self.right_bicepts.limb.body, self.torso.body, radians(-45), 
+                                            radians(70))
+        
+        self.left_shoulder_limit = pm.RotaryLimitJoint(self.left_bicepts.limb.body, self.torso.body, radians(-45),
+                                            radians(70))
+        
+        self.neck_to_torso_limit = pm.RotaryLimitJoint(self.neck.limb.body, self.torso.body, radians(-50), 
+                                            radians(50))
+        
+        self.neck_to_head_limit = pm.RotaryLimitJoint(self.neck.limb.body, self.head.body, radians(-10),
+                                            radians(10))
+
 
         physics_space.add(self.right_elbow_limit)
         physics_space.add(self.left_elbow_limit)
+        physics_space.add(self.right_shoulder_limit)
+        physics_space.add(self.left_shoulder_limit)
+        physics_space.add(self.neck_to_torso_limit)
+        physics_space.add(self.neck_to_head_limit)
 
 
 
@@ -192,7 +207,14 @@ class Character:
         self.hip_limit = pm.RotaryLimitJoint(self.left_leg.limb.body, self.right_leg.limb.body, radians(-120),
                                               radians(120))
         
+        self.body_left_leg_limit = pm.RotaryLimitJoint(self.left_leg.limb.body, self.torso.body, radians(-190),
+                                            radians(0))
+        self.body_right_leg_limit = pm.RotaryLimitJoint(self.right_leg.limb.body, self.torso.body, radians(-190),
+                                            radians(0))
+        
         physics_space.add(self.hip_limit)
+        physics_space.add(self.body_left_leg_limit)
+        physics_space.add(self.body_right_leg_limit)
 
 
         ##################
@@ -217,9 +239,9 @@ class Character:
         self.right_foot.draw(rl.RED)
 
         self.torso.draw(rl.MAROON)
-        self.head.draw(rl.BEIGE)
+        self.head.draw(rl.BROWN)
 
-        self.neck.limb.draw(rl.BEIGE)
+        self.neck.limb.draw(rl.BROWN)
 
         self.left_bicepts.limb.draw(rl.GRAY)
         self.right_bicepts.limb.draw(rl.WHITE)
@@ -232,10 +254,14 @@ class Character:
     def move_legs_q(self) -> None:
         self.left_leg.move_muscle(self.leg_muscle_strength, -radians(50))
         self.right_leg.move_muscle(self.leg_muscle_strength, radians(50))
+        self.right_bicepts.move_muscle(self.leg_muscle_strength, radians(120))
+        self.left_bicepts.move_muscle(self.leg_muscle_strength, radians(-120))
 
     def move_legs_w(self) -> None:
         self.left_leg.move_muscle(self.leg_muscle_strength, radians(50))
         self.right_leg.move_muscle(self.leg_muscle_strength, -radians(50))
+        self.right_bicepts.move_muscle(self.leg_muscle_strength, radians(-120))
+        self.left_bicepts.move_muscle(self.leg_muscle_strength, radians(120))
 
     def move_knees_o(self) -> None:
         self.left_calf.move_muscle(self.leg_muscle_strength, radians(50))
@@ -265,6 +291,8 @@ class Character:
 
     
 
+    
+
 
 def main():
     space = pm.Space()
@@ -282,7 +310,7 @@ def main():
     ]
     ground_shape = pm.Poly(ground_body, ground_poly)
     ground_shape.friction = 0.8
-    space.add(ground_body, ground_shape)
+    #space.add(ground_body, ground_shape)
 
     rl.set_target_fps(60)
 
