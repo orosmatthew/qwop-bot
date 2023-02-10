b8a6a4ba327053ac21ae51ac29ff7a96990ab543
SonnyASmith <sonnyasmith117@gmail.com>
2023-02-09 21:12:03 -0500

    create timer

--- qwop.py
+++ qwop.py
@@ -323,7 +323,8 @@ def main():
         rl.end_mode_2d()
 
         position = character.torso.body.position
-        rl.draw_text(str(round(position[0], 0) ), 20, 0, 50, (153, 204, 255, 255) )
+        rl.draw_text("Distance: " +str(round(position[0], 0) ) + "m", 20, 0, 50, (153, 204, 255, 255) )
+        rl.draw_text("Time: " + str(round(rl.get_time(), 2)  ), 20, 50, 50, (153, 204, 255, 255) )
         
         rl.end_drawing()
     rl.close_window()
