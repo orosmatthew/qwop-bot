c0046ac48e3b610277510c064c8d8f6d2a6dd68e
SonnyASmith <sonnyasmith117@gmail.com>
2023-03-06 22:26:11 -0500

    Create QWOP Visualization.py

---
+++ src/QWOP Visualization.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 21:23:12 2023
+
+@author: wilds
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+pd.set_option('expand_frame_repr', False)
+df = pd.read_json("network.json")
+
+df.head()
+df.describe()
+
+
+norm = pd.json_normalize(df['network'])  #Separate network column
+norm.head()
+
+df = df.drop(['network'], axis = 1)  #Drop original network column
+df.head()
+
+
+new_df = pd.concat([df, norm], axis = 1) #Concat color and normalized network 
+
+new_df.head()
