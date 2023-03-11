ae1c86e01db9fb83853f1e57598352fde94f2477
SonnyASmith <sonnyasmith117@gmail.com>
2023-03-10 22:10:31 -0500

    Update QWOP Visualization.py

--- src/QWOP Visualization.py
+++ src/QWOP Visualization.py
@@ -23,7 +23,27 @@ df.head()
 
 
 new_df = pd.concat([df, norm], axis = 1) #Concat color and normalized network 
-
 new_df.head()
+new_df['bias_ih'].describe()
 
 new_df.plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[0:10].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[10:20].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[20:30].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[30:40].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[40:50].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[50:60].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[60:70].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[70:80].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[80:90].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+new_df[90:100].plot.bar(xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
+
+
+agg_df = pd.DataFrame([norm[0:10].mean(), new_df[10:20].mean(),
+          new_df[20:30].mean(), new_df[30:40].mean(),
+          new_df[40:50].mean(), new_df[50:60].mean(), 
+          new_df[60:70].mean(), new_df[70:80].mean(),
+          new_df[80:90].mean(), new_df[90:100].mean() ])
+          
+          
+agg_df.plot.bar(title = 'Averaged Weights', xlabel = 'Runner #', ylabel = 'Weight', stacked = True)
